cb43e4a14f807d236407566978443c6fdbe908c0
Evgeny Katukhov <e.katuhov@gmail.com>
2023-02-19 21:30:45 +0300

    invalidTest

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -20,7 +20,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Как трудно учиться"));
+                .body("data", equalTo("Как учиться трудно"));
     }
 
 
